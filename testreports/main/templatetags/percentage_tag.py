cc2376eca3841b1d061e67b6c1a3181a77a6f125
Nikita-Goncharov <nikitagoncarov657@gmail.com>
2023-08-25 15:14:18 +0300

    remove print

--- testreports/main/templatetags/percentage_tag.py
+++ testreports/main/templatetags/percentage_tag.py
@@ -5,7 +5,6 @@ register = template.Library()
 
 @register.simple_tag
 def percentage(*args, **kwargs):
-    print(kwargs, args)
     count_in_city = kwargs["count_in_city"]
     all_count = kwargs["all_count"]
     return count_in_city / all_count
